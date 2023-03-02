d93278a19588aba96108aabe9d2bc16e9b812ed6
jhcreed <jordan.h.creed@moffitt.org>
2023-03-02 14:17:02 -0500

    first commit

---
+++ slides/Make PDF of slides.R
@@ -0,0 +1,13 @@
+library(xaringanBuilder)
+
+
+build_pdf(
+  input = "F:/myGitRepo/Intro_to_R_2022/slides/06_slides-stats-2022.Rmd",
+  output_file = "F:/myGitRepo/Intro_to_R_2022/slides/Lecture6slides.pdf",
+  complex_slides = TRUE)
+
+
+build_pdf(
+  input = "F:/myGitRepo/Intro_to_R_2022/slides/07_slides-stats-2022.Rmd",
+  output_file = "F:/myGitRepo/Intro_to_R_2022/slides/Lecture7slides.pdf",
+  complex_slides = TRUE)
